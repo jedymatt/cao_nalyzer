71db4779b68bd005b7ee321d537ac3863267dc55
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-10-18 15:18:03 +0800

    Update home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:cao_nalyzer/screens/take_picture_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
@@ -12,31 +13,101 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  // method to change Navigation bar index
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final views = [
+    const HomeView(),
+    const ResultsView(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () async {
-          final cameras = await availableCameras();
-          final firstCamera = cameras.first;
-
-          if (!mounted) return;
-
-          await Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => TakePictureScreen(camera: firstCamera),
-            ),
-          );
-        },
-        isExtended: true,
-        label: const Text('Scan Cacao Beans'),
-        icon: const Icon(Icons.camera_alt),
-        tooltip: 'Scan Cacao Beans',
-      ),
+      body: views[_selectedIndex],
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.analytics),
+            label: 'Results',
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final ImagePicker _picker = ImagePicker();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Analyze the cacao beans:"),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.camera_alt),
+            onPressed: () async {
+              final cameras = await availableCameras();
+              final firstCamera = cameras.first;
+
+              if (!mounted) return;
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TakePictureScreen(
+                    camera: firstCamera,
+                  ),
+                ),
+              );
+            },
+            label: const Text("Take a picture"),
+          ),
+          const Text('or'),
+          OutlinedButton.icon(
+            icon: const Icon(Icons.image),
+            onPressed: () async {
+              final XFile? pickedImage =
+                  await _picker.pickImage(source: ImageSource.gallery);
+            },
+            label: const Text('Select an image'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ResultsView extends StatelessWidget {
+  const ResultsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
